493890c348b69dff38f19e2ba5541d0773cf1927
iwanbazz <iwan.bazz@gmail.com>
2020-06-22 13:51:31 +0700

    Add auth page

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,27 @@ const routes = [
       { path: "/help", component: () => import("pages/Help.vue") }
     ]
   },
+  {
+    path: "/auth",
+    component: () => import("layouts/AuthLayout.vue"),
+    children: [
+      {
+        path: "forgotPassword",
+        name: "ForgotPassword",
+        component: () => import("pages/ForgotPassword.vue")
+      },
+      {
+        path: "login",
+        name: "Login",
+        component: () => import("pages/Auth.vue")
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: () => import("pages/Auth.vue")
+      }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
